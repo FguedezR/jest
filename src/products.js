7546c0431fcc1ef5342df59a84605c07c664c277
Fernando <fernanguedezr@gmail.com>
2026-03-22 18:50:50 +0100

    Update products.js

--- src/products.js
+++ src/products.js
@@ -5,3 +5,33 @@ function resetProducts() {
   products = [];
   id = 0;
 }
+
+
+function addProduct() {
+  if (!name || !price) {
+    throw new Error("Name and price are required");
+  }
+  id++;
+  const newProduct = { id, name, price };
+  products.push(newProduct);
+  return newProduct;
+}
+
+function remoreProduct(idToRemove) {
+  const index = products.findIndex((p) => p.id === idToRemove);
+  if (index === -1) {
+    throw new Error("Product not found");
+  }
+  products.splice(index, 1);
+}
+
+function getProducts() {
+  return products;
+}
+
+function getProduct(idToFind) {
+    const product = products.find(p => p.id === idToFind)
+    if (!product) {
+        throw new Error("Product not found")
+    }
+}
